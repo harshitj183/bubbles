662e68b96eedbb2c4c7be10f916b3661fb134fd0
Harshit Jaiswal <76927137+harshitj183@users.noreply.github.com>
2024-12-09 22:57:42 +0530

    done

--- main.ts
+++ main.ts
@@ -1,42 +1,25 @@
-// Create the player sprite
-let player = sprites.create(img`
-    . . . . . 4 4 4 4 . . . . . 
-    . . . 4 4 4 4 4 4 4 . . . . 
-    . . 4 4 4 4 4 4 4 4 4 . . . 
-    . 4 4 4 4 4 4 4 4 4 4 4 . . 
-    4 4 4 4 4 4 4 4 4 4 4 4 4 . 
-    . . . . 4 4 4 4 . . . . . . 
-    . . . . 4 4 4 4 . . . . . . 
-    . . . . . 4 4 . . . . . . . 
-    `, SpriteKind.Player);
-
-// Set player controls
-controller.moveSprite(player, 100, 100);
-player.setStayInScreen(true);
-
-// Add bubbles
-game.onUpdateInterval(1000, function () {
-    let bubble = sprites.create(img`
-        . . . . 2 2 2 2 2 . . . . 
-        . . 2 2 2 2 2 2 2 2 . . . 
-        . 2 2 2 2 2 2 2 2 2 2 . . 
-        2 2 2 2 2 2 2 2 2 2 2 2 . 
-        2 2 2 2 2 2 2 2 2 2 2 2 . 
-        . 2 2 2 2 2 2 2 2 2 2 . . 
-        . . 2 2 2 2 2 2 2 2 . . . 
-        . . . . 2 2 2 2 2 . . . . 
-        `, SpriteKind.Enemy);
-
-    bubble.setVelocity(0, 50);
-    bubble.setPosition(Math.randomRange(10, 150), 0);
-});
-
 // Destroy bubbles on overlap
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
-    otherSprite.destroy(effects.disintegrate, 100);
-    info.changeScoreBy(1);
-});
-
+    otherSprite.destroy(effects.disintegrate, 100)
+    info.changeScoreBy(1)
+})
+let bubble2: Sprite = null
+// Create the player sprite
+let player = sprites.create(img`
+    . . . . . . 2 2 . . . . . . 
+    . . . . 2 2 2 2 2 2 . . . . 
+    . . . 2 2 2 2 2 2 2 2 . . . 
+    . . 2 2 2 2 2 2 2 2 2 2 . . 
+    2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+    . 2 . . . . 2 2 . . . . 2 . 
+    . 2 . . 2 2 2 2 2 2 . . 2 . 
+    . . . . 2 . . . . 2 . . . . 
+    `, SpriteKind.Player)
+// Set player controls
+controller.moveSprite(player, 100, 100)
+player.setStayInScreen(true)
+// Initialize score
+info.setScore(0)
 // Game over if bubbles reach bottom
 game.onUpdate(function () {
     sprites.allOfKind(SpriteKind.Enemy).forEach(function (bubble) {
@@ -44,8 +27,19 @@ game.onUpdate(function () {
             game.over(false);
         }
     });
-});
-
-// Initialize score
-info.setScore(0);
-
+})
+// Add bubbles
+game.onUpdateInterval(1000, function () {
+    bubble2 = sprites.create(img`
+        . . 6 . . 6 6 6 . . 6 . . 
+        6 . . 6 . . 6 . . 6 . . 6 
+        6 . . . 6 . 6 . 6 . . . 6 
+        6 6 6 6 6 6 6 6 6 6 6 6 6 
+        6 6 6 6 6 6 6 6 6 6 6 6 6 
+        6 . . . 6 . 6 . 6 . . . 6 
+        . . . 6 . . 6 . . 6 . . . 
+        . . 6 . . 6 6 6 . . 6 . . 
+        `, SpriteKind.Enemy)
+    bubble2.setVelocity(0, 50)
+    bubble2.setPosition(Math.randomRange(10, 150), 0)
+})
